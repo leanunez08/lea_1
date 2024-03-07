bd554ff7a9abf93c2da2406208cb38117b4c2fe8
Leandro <162610082+leanunez08@users.noreply.github.com>
2024-03-07 14:07:03 -0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,25 @@
+x=1
+while x<=100:
+    print(x)
+    x=x+1
+
+
+x=1
+while x<=500:
+    print(x)
+    x=x+1
+
+x=50
+while x<=100:
+    print(x)
+    x=x+1
+
+x=-50
+while x<=0:
+    print(x)
+    x=x+1
+
+x=2
+while x<=100:
+    print(x)
+    x=x+2
